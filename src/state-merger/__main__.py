6407d0cd69bf9146671931a86f4c5b00b244f20d
Brian Li <115197764+ShabbyGayBar@users.noreply.github.com>
2026-01-30 09:16:16 +0800

    change file structure to standard package

--- src/state-merger/__main__.py
+++ src/state-merger/__main__.py
@@ -3,19 +3,21 @@ import os
 import json
 
 this_dir = os.path.dirname(__file__)
-game_root_dir = "C:/Program Files (x86)/Steam/steamapps/common/Victoria 3/game"+'/'
-merge_file = f'{this_dir}/merge_states.json'
+game_root_dir = "C:/Program Files (x86)/Steam/steamapps/common/Victoria 3/game" + "/"
+merge_file = f"{this_dir}/merge_states.json"
+
 
 def main():
     # Read merge state plan
-    with open(merge_file, 'r', encoding='utf-8') as file:
+    with open(merge_file, "r", encoding="utf-8") as file:
         merge_dict = json.load(file)
 
     # Merge states
-    state_merger = StateMerger(game_root_dir, f'{this_dir}/mod/', merge_dict, f'{this_dir}/data/')
+    state_merger = StateMerger(
+        game_root_dir, f"{this_dir}/mod/", merge_dict, f"{this_dir}/data/"
+    )
     state_merger.merge_state_data(buff=False, ignoreSmallStates=True)
     state_merger.merge_misc_data()
     state_merger.merge_loc_data()
 
-if __name__ == "__main__":
-    main()
+main()
